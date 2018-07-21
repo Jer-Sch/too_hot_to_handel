37621b28d44714e5ad751e90baab04a00311b502
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-07-21 11:15:58 -0700

    Refactoring CLI

--- lib/too_hot_to_handel/cli.rb
+++ lib/too_hot_to_handel/cli.rb
@@ -1,16 +1,72 @@
 class TooHotToHandel::CLI
 
+#   def call
+#     puts "Hello! This is Too Hot to Handel! Bringing classical music reviews right to your command line!"
+#     puts ""
+#
+#     TooHotToHandel::Scraper.scrape_classical_reviews
+#
+#     command
+#   end
+#
+#   def command
+#     puts "If you please, select a review to read:"
+#     puts ""
+#
+#     reviews = TooHotToHandel::ClassicalReview.all
+#
+#     reviews.each.with_index(1) do |review, index|
+#       puts "#{index} - #{review.title}"
+#       puts ""
+#       puts "#{review.description}"
+#       puts ""
+#     end
+#
+#     input = gets.strip
+#     index = input.to_i-1
+#
+#     classical_review = TooHotToHandel::ClassicalReview.all[index]
+#
+#     TooHotToHandel::Scraper.scrape_review_content(classical_review)
+#
+#     puts classical_review.content
+#
+#     puts "Please select another review to read:"
+#
+#
+#
+#   end
+# end
+
   def call
-    puts "Hello! This is Too Hot to Handel! Bringing classical music reviews right to your command line!"
-    puts ""
+    user_input = ""
 
-    TooHotToHandel::Scraper.scrape_classical_reviews
+    while user_input != "exit"
+      puts "Hello! This is Too Hot to Handel! Bringing classical music reviews right to your command line!"
+      puts ""
+      puts "To view the latest classical music reviews, type 'show me'"
+      puts ""
+      puts "After reading a review, type 'more' to see the list again."
+      puts ""
+      puts "To read any review in your web browser, type 'web'."
+      puts ""
+      puts "To quit at any time, type 'exit'."
+      puts ""
+      puts "Are you ready?"
 
-    command
+      user_input = gets.strip
+
+      case user_input
+      when "show me"
+        list_reviews
+      when "more"
+        list_reviews
+      end
+    end
   end
 
-  def command
-    puts "If you please, select a review to read:"
+  def list_reviews
+    puts "Select a review you would like to read:"
     puts ""
 
     reviews = TooHotToHandel::ClassicalReview.all
@@ -20,8 +76,11 @@ class TooHotToHandel::CLI
       puts ""
       puts "#{review.description}"
       puts ""
-    end
 
+    end
+  end
+
+  def view_content
     input = gets.strip
     index = input.to_i-1
 
@@ -30,10 +89,5 @@ class TooHotToHandel::CLI
     TooHotToHandel::Scraper.scrape_review_content(classical_review)
 
     puts classical_review.content
-
-    puts "Please select another review to read:"
-
-    
-
   end
 end
