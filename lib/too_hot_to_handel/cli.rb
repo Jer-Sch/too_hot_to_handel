70eb289903eed0a52e2c62ee8858726720ebd227
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-07-21 13:04:35 -0700

    Further refinements to browser opening capability

--- lib/too_hot_to_handel/cli.rb
+++ lib/too_hot_to_handel/cli.rb
@@ -9,7 +9,9 @@ class TooHotToHandel::CLI
 
     while user_input != "exit"
       puts ""
-      puts "To view the latest classical music reviews, type 'show me'"
+      puts "To view the latest classical music reviews in your command line, type 'show me'"
+      puts ""
+      puts "To view the reviews in your browser, type 'show me on the web'"
       puts ""
       puts "To quit at any time, type 'exit'."
       puts ""
@@ -21,8 +23,8 @@ class TooHotToHandel::CLI
       case user_input
       when "show me"
         list_reviews
-      when "web"
-        Launchy.open("https://www.nytimes.com/spotlight/classical-music-reviews")
+      when "show me on the web"
+        view_content_in_browser
       else
         puts ""
         puts "I didn't understand that. Please try again."
@@ -45,20 +47,30 @@ class TooHotToHandel::CLI
       puts "#{review.description}"
       puts ""
     end
-    view_content
+    view_content_in_browser
     TooHotToHandel::ClassicalReview.destroy_all
   end
 
-  def view_content
+  # def view_content
+  #   input = gets.strip
+  #   index = input.to_i-1
+  #
+  #   classical_review = TooHotToHandel::ClassicalReview.all[index]
+  #
+  #   TooHotToHandel::Scraper.scrape_review_content(classical_review)
+  #
+  #   puts classical_review.content
+  #   puts "------------------------------------------------------"
+  #   end
+  # end
+
+  def view_content_in_browser
     input = gets.strip
     index = input.to_i-1
 
     classical_review = TooHotToHandel::ClassicalReview.all[index]
-
     TooHotToHandel::Scraper.scrape_review_content(classical_review)
-
-    puts classical_review.content
-    puts "------------------------------------------------------"
-    end
+    Launchy.open("#{classical_review.url}")
   end
+end
 end
