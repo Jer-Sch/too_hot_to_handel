4ecd5780495e5aaaff700e48767b16175446bb90
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-07-20 23:00:07 -0700

    Refining scraper issues. Previous scraper would not iterate. It was due to a typo

--- lib/too_hot_to_handel/cli.rb
+++ lib/too_hot_to_handel/cli.rb
@@ -6,5 +6,17 @@ class TooHotToHandel::CLI
 
     TooHotToHandel::Scraper.scrape_classical_reviews
 
+    command
+  end
+
+  def command
+    puts "If you please, select a review to read:"
+    puts ""
+
+    reviews = TooHotToHandel::ClassicalReview.all
+
+    reviews.each.with_index(1) do |review, index|
+      puts "#{index} - #{review.title}"
+    end
   end
 end
--- lib/too_hot_to_handel/scraper.rb
+++ lib/too_hot_to_handel/scraper.rb
@@ -5,17 +5,25 @@ class TooHotToHandel::Scraper
   def self.scrape_classical_reviews
     doc = Nokogiri::HTML(open(BASE_URL))
 
-    doc.css(".css-1cp3ece").each do |classical_review|
-      title = doc.css(".css-p4c7a9").text
-      description = doc.css(".css-1up1n27").text
-      url = "https://www.nytimes.com" + doc.css(".css-1xc9qte a").attr("href")
+
+    doc.css(".css-1cp3ece").each do |review|
+      title = review.css(".css-p4c7a9").text
+      description = review.css(".css-1up1n27").text
+      url = "https://nytimes.com" + review.css(".css-1xc9qte a").attr("href")
       TooHotToHandel::ClassicalReview.new(title, url)
+
     end
   end
 
   def self.scrape_review_content(classical_review)
     url = classical_review.url
     doc = Nokogiri::HTML(open(url))
+
+  end
+
+
+
+
 
 
 end
